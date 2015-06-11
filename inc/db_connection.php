40091dde8c3395625b71742e56fac263bb589ca9
adamzuckerberg <adam@adamzuckerberg.com>
2015-06-11 12:51:01 -0700

    Update config for heroku

--- inc/db_connection.php
+++ inc/db_connection.php
@@ -1,4 +1,8 @@
 <?php
+  define("DB_SERVER", "localhost");
+  define("DB_USER", "bcfe0905177bc7");
+  define("DB_PASS", "d4101cabd05fa72");
+  define("DB_NAME", "heroku_91162cf5ac94102");
 
 // for the live version to spa db
   // define("DB_SERVER","localhost");
@@ -8,31 +12,13 @@
   // define("DB_PORT","3306"); // default: 3306, Randy uses 8889
 
   // 1. Create a database connection
-  // $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
-  // // Test if connection succeeded
-  // if(mysqli_connect_errno()) {
-  //   echo "all good";
-  //   die(" Database connection failed: " . 
-  //        mysqli_connect_error() . 
-  //        " (" . mysqli_connect_errno() . ")"
-  //   );
-  // }
-
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-DB_SERVER = $url["host"];
-DB_USER = $url["user"];
-DB_PASS = $url["pass"];
-DB_NAME = substr($url["path"], 1);
-
-$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
   // Test if connection succeeded
   if(mysqli_connect_errno()) {
     echo "all good";
-    die(" Database connection failed: " . 
+    die("Database connection failed: " . 
          mysqli_connect_error() . 
          " (" . mysqli_connect_errno() . ")"
     );
   }
-
 ?>
