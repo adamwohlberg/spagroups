2bf36a60fc13e0d6e0a6de529654ee24b683541d
adamzuckerberg <adam@adamzuckerberg.com>
2015-06-11 12:41:16 -0700

    Configure db connection

--- inc/db_connection.php
+++ inc/db_connection.php
@@ -1,8 +1,4 @@
 <?php
-  // define("DB_SERVER", "localhost");
-  // define("DB_USER", "bcfe0905177bc7");
-  // define("DB_PASS", "d4101cabd05fa72");
-  // define("DB_NAME", "heroku_91162cf5ac94102");
 
 // for the live version to spa db
   // define("DB_SERVER","localhost");
@@ -24,12 +20,12 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+  define("DB_SERVER", $url["host"]);
+  define("DB_USER", $url["user"];
+  define("DB_PASS", $url["pass"]);
+  define("DB_NAME", substr($url["path"], 1);
 
-$connection = mysqli_connect($server, $username, $password, $db);
+$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
   // Test if connection succeeded
   if(mysqli_connect_errno()) {
     echo "all good";
