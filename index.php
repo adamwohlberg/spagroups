75f3ac1e261eedbcc01b0127a599acfab06df0c2
adamzuckerberg <adam@adamzuckerberg.com>
2015-06-11 13:27:32 -0700

    Switch splash to root.  Link to index now goes to home.php

--- index.php
+++ index.php
@@ -1,171 +1,220 @@
-<?php ob_start(); ?>
-<?php
-$pageTitle = 'All-Inclusive Spa Resort Discounts';
-include("inc/header.php");
-include("inc/products.php");
-// confirm_logged_in();
-?>
-    <div id="body" style="margin:5px 0 0 0">
-      <div class="upcomingGroupsText"> Save Up to <span style="color:#ff68fd; font-size:32px">50%</span> On These Upcoming Groups </div>    
-      <div id="sortButtons" style="display:inline-block; vertical-align:text-bottom">
-      <button class="button" style="display:inline-block" title="click to view all groups" alt="click to view all spa groups">All</button>
-      <button class="button2" style="display:inline-block" title="click to view groups expiring soon" alt="click to view spa groups expiring soon">Expiring Soon</button>
-      <button class="button3" style="display:inline-block" title="click to view only active deals" alt="click to view only active deals">Active Deals</button>
-      </div>   
-      <div style="margin:5px 35px 0 20px">
-      <div class="ads" id="right" style="float:right;" >
-        <div style="height:240px; width:217px">
-          <div style="padding:0px 0 0 0px; margin:-20px 0px 0px 0px; color: grey; font-weight:1.2em">Inspired Yoga & Spa Apparel:</div>
-          <div><a href="http://www.beloveapparel.com/women/"><img id="ad1" src="images/belove2.png" height="200px" width="217px" border="1px" float="left"></a></div>
-        </div>
-        <div style="height:240px; width:217px">
-          <div style="padding:12px 0 0 0px; margin:-20px 10px 0px 0px; color: grey; font-weight:1.2em"></div>
-          <div><a href="http://www.beloveapparel.com/women/"><img id="ad2" src="images/belove.png" height="200px" width="217px" border="1px" float="left"></a></div>
-        </div>
-          <div style="width:202px; height:272px; background-color:#FFF; border:solid 1px #FFF; margin:10px 0; padding:10px 5px 0px 0px;">
-            <div id="slider" class="nivoSlider"> <img src="images/review1.png" alt="" /> <img src="images/review2.png" alt="" /> <img src="images/review3.png" alt="quotes" /> </div>
-          </div>
-        <div style="height:240px; width:111px; margin: -10px 0px 0px 0px;">
-          <div><img src="images/iSpaColorMember.jpg" id="ad4" height="156px" width="89px" border="0px" float="left"/></div>
-          <img id="iatan" src="images/IATAN.jpg" height="111px" width="111px"></img>
-        </div>
-<!--         <div style="height:300px; width:217px; margin: -10px 0px 0px 0px;">
-          <div style="padding:0px 0 10px 0px; margin: 0px 0px 0px 0px; color: grey; font-weight:1.2em">We Recommend:</div>
-          <div style="padding:12px 0 0 0px; margin:-20px 10px 0px 0px; color: grey; font-weight:1.2em"></div>
-          <div><a href="http://www.yourpurpose.com"><img src="images/baeth_ad.jpg" id="ad3" height="200px" width="217px" border="1px" float="left"></a></div>
-        </div> -->
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+<!-- Google Analytics Tracking Code -->
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-47677665-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script>
+alert('Thank you for three years of destination spa retreats. Please note that SpaGroups is no longer in operation.');
+</script>
+<!-- Google Analytics Tracking Code -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<!--[if !IE 7]>
+	<style type="text/css">
+		#wrap {display:table;height:100%}
+	</style>
+<![endif]-->
+<title>SpaGroups | VIP List - All-Inclusive Spa Resort Discounts </title>
+<!--favicon refresh -->
+<link rel="shortcut icon" href="https://www.spagroups.com/favicon.ico?v=2" />
+</head>
+
+<style> 
+html {
+  background: url(images/sauna.png)no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+#container {
+  width: 100%;
+  height: 100%;
+}
+#skip {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1em;
+  font-style: bold;
+  color: #ff68fd;
+  width: 100%;
+  text-align: right;
+  margin-top: 1px;
+}
+#skip a {
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+}
+#skip a:hover {
+  font-family: Arial, Helvetica, sans-serif;
+  text-decoration: none;
+  color: #f11fff;
+} 
+.footer {
+  width:100%;
+  bottom: 2%;
+  font-size: .7em;
+  color: #ff68fd;
+  position:absolute;
+  background-color: none;
+  height:80px;
+  font-family:Arial, Helvetica, sans-serif;
+  clear:both;
+  text-align:left;
+}
+.footer a {
+  color: #ff68fd;
+  text-decoration:none;
+}
+.footer a:hover {
+  color: #fdd5fd;
+  text-decoration:underline;
+}
+.background {
+width: 535px; 
+height: 315px;
+padding: 2px;
+background: rgb(69,72,77); /* Old browsers */
+background: -moz-linear-gradient(top,  rgba(69,72,77,1) 0%, rgba(0,0,0,1) 7%); /* FF3.6+ */
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(69,72,77,1)), color-stop(7%,rgba(0,0,0,1))); /* Chrome,Safari4+ */
+background: -webkit-linear-gradient(top,  rgba(69,72,77,1) 0%,rgba(0,0,0,1) 7%); /* Chrome10+,Safari5.1+ */
+background: -o-linear-gradient(top,  rgba(69,72,77,1) 0%,rgba(0,0,0,1) 7%); /* Opera 11.10+ */
+background: -ms-linear-gradient(top,  rgba(69,72,77,1) 0%,rgba(0,0,0,1) 7%); /* IE10+ */
+background: linear-gradient(to bottom,  rgba(69,72,77,1) 0%,rgba(0,0,0,1) 7%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#45484d', endColorstr='#000000',GradientType=0 ); /* IE6-9 */
+
+}
+.content {
+  text-align: center;
+  color: #ffffff
+}
+.headline {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2em;
+  font-weight:bold;
+    margin-top: .3em;
+  margin-bottom: 0.1em;
+}
+#tagline {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.5em;
+}
+.vipList {
+  font-size: 1.1em;
+  width: 100%;
+  text-align: center;
+  margin: 8px 0px -12px 0px;
+}
+.spamline {
+  font-size: .7em;
+  width: 100%;
+  text-align: center;
+  padding: 0px 20px 10px 20px;
+}
+#image {
+  margin: .2em 0em 0em 0em;
+  width: 220px;
+  height: 40px;
+}
+.boxAndSkip {
+  background-color: none;
+  width: 500px;
+  height: 320px;
+  margin: 15% 34%;
+/*  margin: 14% 0% 0% 32%; */
+}
+input[type=text]:focus { 
+background-color: #ffb1f9;
+text-align: left;
+}
+input[type=text] {
+ font-size: 1.4em;
+/* font-weight: bold;*/
+ width: 65%;
+ height: 1.4em;
+  border-radius: 5px;
+ background-color: white;
+ font-family: Arial, Helvetica, sans-serif;
+ text-align: center;
+}
+input[type=submit] {
+  border-color: gray;
+  border:solid 2px;
+ font-weight: bolder;
+  border-radius: 15px;
+    box-shadow: 4px 4px 0px 0px;
+ font-size: 1.3em;
+ width: 35%;
+ height: 1.8em;
+ margin: 5px 0px 0px 0px;
+ font-style: bold;
+ min-height: 40px;
+}
+input[type=submit]:hover {
+  background-color: hotpink;
+}
+/*media queries*/
+@media screen and (max-width: 575px) {
+.background {
+width: 250px; 
+height: 250px; 
+}
+.boxAndSkip {
+  background-color: none;
+  width: 250px;
+  height: 250px;
+  margin: 30% auto;
+}
+input[type=text] {
+ font-size: 1.1em;
+ width: 90%;
+}
+input[type=submit] {
+ font-size: 1.1em;
+ width: 20%;
+}
+</style>
+
+<body>
+<div id="container">
+  <div class="filler"></div>
+  <div class="boxAndSkip">
+      <div class="background">
+          <div class="content">
+            <p class="headline">The Best Kept Secret in <br>Luxury Spa Travel</p>
+            <span id="tagline" >Join a Group &amp; Save Up to 50% </span>
+            <div><image id="image" src="images/splashlogo.png"></div>
+              <form method="post" action="http://www.aweber.com/scripts/addlead.pl ">
+              <input type="hidden" name="listname" value="spagroups_basic" />
+              <input type="hidden" name="redirect" value="http://www.spagroups.com/vip-thankyou.php" />
+              <input type="hidden" name="meta_adtracking" value="custom form" />
+              <input type="hidden" name="meta_message" value="1" /> 
+              <input type="hidden" name="meta_required" value="email" /> 
+              <input type="hidden" name="meta_forward_vars" value="1" /> 
+              <input type="text" onfocus="if(this.value == 'email@address.com') { this.value = ''; }" name="email" value="email@address.com" /> 
+              <input style="padding:5px 0px 0px 0px;" type="submit" name="submit" value="Join the VIP List" /> 
+              </form>
+              <p class="vipList">Enter your email to be notified of last-minute deals.</p> 
+              <br>
+            <span class="spamline">We protect your privacy and will not share your information with any other organization. </span>
+          </div>
       </div>
-      <div style="float:none;">
-        <div class="demo upcomingGroups">
-<!--           removed style="list-style-type: none;" from line below -->
-            <ul id="every" class="thumbs">
-              <?php foreach($groups as $group_id => $group) { ?>
-              <?php
-              $now = time(); // or your date as well
-              $counter = ($group["groupRatesAt"]-$group["registered"]);
-              $arrivalDate = strtotime($group["arrival"]);
-              $createdDate = strtotime($group["created"]);
-              $dateDiff = abs($now - $arrivalDate);
-              $dateDiff2 = floor($dateDiff/(60*60*24));
-              $createdDateDiff = abs($now - $createdDate);
-              $createdDateDiff2 = floor($createdDateDiff/(60*60*24));
-              if ($counter<1) {
-              $class = "active";
-              } elseif ($dateDiff2<60) {
-              $class = "expiring";
-              } elseif ($createdDateDiff2<30) {
-              $class = "new";
-              } else {
-              $class = "plain";
-              }
-              ?>
-              <?php 
-              echo "<li data-id='$group_id' class='$class all'>"; 
-              ?>
-              <a href="group.php?id=<?php echo $group_id; ?>">
-              <div class="roundedImage" style="height:263px; width:419px; float:left; background-image:url('<?php echo $group["image"]; ?>'); cursor:normal">                     
-                <?php 
-                $counter = ($group["groupRatesAt"]-$group["registered"]); 
-                if ($counter <= 0) {
-                $counter = 0;
-                } else {
-                $counter = ($group["groupRatesAt"]-$group["registered"]);
-                } 
-                ?>
-                <div><img class="counterFlag" src="<?php echo 'images/banner-' .$counter.'.png'; ?>" /></div>
-                <div class="btnJoin"><a href="group.php?id=<?php echo $group_id; ?>" style="text-decoration:none; color:white; display:block; height:100%; width:100%">View Details</a></div>
-                <div class="roundedBottom" style="margin:83px 0 0 0; background-image:url(images/blackbottom.png); height:66px; width:420px; color:#FFF; font-size:22px; cursor:default">
-                  <div style="float:left; margin:9px 0 0 10px;" class="links"><a href="group.php?id=<?php echo $group_id; ?>"><?php echo $group["spa"]; ?></a><br />
-                    <span style="font-size:16px; color:#CCC"><?php echo $group["citystate"]; ?></span></div>
-                  <div style="float:right; margin:9px 10px 0 0px; text-align:right"><span style="font-size:11px; color:#CCC">Arrival:</span> <span class="links"><a href="group.php?id=<?php echo $group_id; ?>"><?php echo $group["arrival"]; ?></a><br  />
-                    <span style="font-size:11px; margin:-5px 0 0 0; color:#CCC">Expires in: <span style="font-size:16px; color:#ff68fd">
-                    <?php
-                     $now = time(); // or your date as well
-                     $arrivalDate = strtotime($group["arrival"]);
-                     $datediff = abs($now - $arrivalDate);
-                     echo floor($datediff/(60*60*24)) . " days";
-                    ?>
-                  </div>
-                </div>
-              </div>
-              </a>
-                <?php echo "</li>"; ?>
-                <?php } ?>
-            </ul>
-
-<!-- test for quicksand. this is a duplicate foreach loop (same as one above) but with a different ul data tage and display: none-->
-            <ul id="data" style="display: none;" class="thumbs">
-              <?php foreach($groups as $group_id => $group) { ?>
-              <?php
-              $now = time(); // or your date as well
-              $counter = ($group["groupRatesAt"]-$group["registered"]);
-              $arrivalDate = strtotime($group["arrival"]);
-              $createdDate = strtotime($group["created"]);
-              $dateDiff = abs($now - $arrivalDate);
-              $dateDiff2 = floor($dateDiff/(60*60*24));
-              $createdDateDiff = abs($now - $createdDate);
-              $createdDateDiff2 = floor($createdDateDiff/(60*60*24));
-              if ($counter<1) {
-              $class = "active";
-              } elseif ($dateDiff2<60) {
-              $class = "expiring";
-              } elseif ($createdDateDiff2<30) {
-              $class = "new";
-              } else {
-              $class = "all2";
-              }
-              ?>
-              <?php 
-              echo "<li data-id='$group_id' class='$class all'>"; 
-              ?>
-              <a href="group.php?id=<?php echo $group_id; ?>">
-              <div class="roundedImage" style="height:263px; width:419px; float:left; background-image:url('<?php echo $group["image"]; ?>'); cursor:normal">                     
-                <?php 
-                $counter = ($group["groupRatesAt"]-$group["registered"]); 
-                if ($counter <= 0) {
-                $counter = 0;
-                } else {
-                $counter = ($group["groupRatesAt"]-$group["registered"]);
-                } 
-                ?>
-                <div><img class="counterFlag" src="<?php echo 'images/banner-' .$counter.'.png'; ?>" /></div>
-                <div class="btnJoin"><a href="group.php?id=<?php echo $group_id; ?>" style="text-decoration: none; color: white; display:block; height:100%; width:100%">View Details</a></div>
-                <div class="roundedBottom" style="margin:83px 0 0 0; background-image:url(images/blackbottom.png); height:66px; width:420px; color:#FFF; font-size:22px; cursor:default">
-                  <div style="float:left; margin:9px 0 0 10px;" class="links"><a href="group.php?id=<?php echo $group_id; ?>"><?php echo $group["spa"]; ?></a><br />
-                    <span style="font-size:16px; color:#CCC"><?php echo $group["citystate"]; ?></span></div>
-                  <div style="float:right; margin:9px 10px 0 0px; text-align:right"><span style="font-size:11px; color:#CCC">Arrival:</span> <span class="links"><a href="group.php?id=<?php echo $group_id; ?>"><?php echo $group["arrival"]; ?></a><br  />
-                    <span style="font-size:11px; margin:-5px 0 0 0; color:#CCC">Expires in: <span style="font-size:16px; color:#ff68fd">
-                    <?php
-                     $now = time(); // or your date as well
-                     $arrivalDate = strtotime($group["arrival"]);
-                     $datediff = abs($now - $arrivalDate);
-                     echo floor($datediff/(60*60*24)) . " days";
-                    ?>
-                  </div>
-                </div>
-              </div>
-              </a>
-                <?php echo "</li>"; ?>
-                <?php } ?>
-            </ul>
-
-          </div>
-        </div>
-<br class="clearfloat" />
+      <p id="skip"><a href="home.php">Skip to Website > </a></p>
   </div>
-
-
-<!--     <div id="disclaimer">
-        <p> <strong>Notice of Non-Affiliation: </strong>Although spagroups provides <span style="font-weight: bold;">all-inclusive spa resort discounts</span> to our members, we are not affiliated with any particular spa resort or destination spa. We want you to have the best <span style="font-weight: bold;">healthy spa vacation</span> at the destination spa or resort of your choice.  If a group member chooses to create a group at Miraval Spa Resort&trade;, Rancho La Puerta&trade;, The Oaks at Ojai&trade;, Lake Austin Resort and Spa&trade; or any other all-inclusive spa resort, we will list the name of the all-inclusive resort or destination spa; however we are in no way affiliated with that particular resort.  Yes, you may obtain Rancho La Puerta&trade; discounts, Miraval&trade; discounts, etc. based on each spa resort's group discount guidelines; however, we have no affiliation and do not set rates at hotels - we simply help our members get the best rates possible by joining and starting groups to leverage group buying power.  We may provide links to the group's Facebook&trade; page, but we are in no way affiliated with Facebook&trade;. Facebook&trade; is a licensed trademark of Facebook&trade;, Miraval Resort & Spa&trade; is a licensed trademark of Miraval Resorts&trade;, Rancho La Puerta&trade; is a licensed trademark of Rancho La Puerta&trade;, The Oaks at Ojai&trade; is a licensed trademark of The Oaks at Ojai&trade;, The Ranch at Live Oak Malibu&trade; is a licensed trademark of The Ranch at Live Oak Malibu&trade; and our SpaGroups members are awesome!</p>
-    </div>  -->
 </div>
 
+<div class="footer">
+  <div style="padding: 65px 0 0 10px"> <a href="privacy.php">Privacy</a> | <a href="terms.php">Terms of Use</a> <span> &copy;2014 SpaGroups, LLC </span> </div>
 </div>
-
-  <?php include('inc/footer.php'); ?>
-
-
-
-
-
+</body>
+</html>
